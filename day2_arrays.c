0c564775c4b57379ce3dc4ec1a653abca1e1ca5d
rakshitaworld-source <rakshitaworld@gmail.com>
2026-06-13 05:27:25 +0530

    completed day2 tasks

--- day2_arrays.c
+++ day2_arrays.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
 int main() {
+    // The tells the hardware to allocate 3 sequential slots in RAM
     int scores = {10, 20, 30};
 
     printf("==================================================\n");
     printf("[TRACKING] Hardware Array Memory Layout\n");
     printf("==================================================\n");
     
-    // Printing the raw hexadecimal memory addresses using the %p formatter
     printf("Element 0: Value = %d, Address = %p\n", scores, (void*)&scores);
     printf("Element 1: Value = %d, Address = %p\n", scores, (void*)&scores);
     printf("Element 2: Value = %d, Address = %p\n", scores, (void*)&scores);
     
     printf("--------------------------------------------------\n");
     printf("Array Name Pointer Direct Read: %p\n", (void*)scores); 
-    // This will match Element 0 perfectly!
     
     return 0;
 }
