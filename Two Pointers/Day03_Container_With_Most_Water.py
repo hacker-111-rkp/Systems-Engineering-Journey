2eb7cfaebc71178473760d40f4812da7074e5891
hacker-111-rkp <hackerkp76@gmail.com>
2026-06-17 04:31:34 +0530

    Update Day03_Container_With_Most_Water.py

--- Two Pointers/Day03_Container_With_Most_Water.py
+++ Two Pointers/Day03_Container_With_Most_Water.py
@@ -1,1 +1,16 @@
-
+class Solution:
+    def maxArea(self, heights):
+        left,right=0,len(heights)-1 
+        max_area=0
+        while left<right:
+            area=min(heights[left],heights[right])*(right-left)
+            max_area=max(max_area,area)
+            if heights[right]>heights[left]:
+                left+=1
+            else:
+                right-=1
+        return max_area
+    
+sol=Solution()
+print(sol.maxArea([1,7,2,5,4,7,3,6]))
+        
