fd8b2e02d18227a2902ab4fbf86a6e68d7b59bf8
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-01 17:56:45 +0530

    Update and rename Day06_Find_Numbers_with_Even_Number_of_Digits,py to Day06_Find_Numbers_with_Even_Number_of_Digits.py

--- Arrays_And_Hashing/Day06_Find_Numbers_with_Even_Number_of_Digits.py
+++ Arrays_And_Hashing/Day06_Find_Numbers_with_Even_Number_of_Digits.py
@@ -7,8 +7,8 @@ class Solution(object):
         return count
 sol=Solution()
 print(sol.findNumbers([55,66,234,222222]))
-"""Time Complexity: $O(N \cdot K)$, where $N$ is the number of elements and $K$ is the number of digits
-in the numbers (due to string conversion).Space Complexity: $O(K)$ to store the string representation of the current number."""
+"""Time Complexity:O(Nk), where N is the number of elements and k is the number of digits
+in the numbers (due to string conversion).Space Complexity: O(K) to store the string representation of the current number."""
 
 #mathematical approach 
 
@@ -23,7 +23,9 @@ def findNumbers(nums):
         if digits % 2 == 0:
             count += 1
     return count
-
+"""Time Complexity:O(Nk) where K is the number of digits.
+Space Complexity: O(1) (no extra storage like strings).
+"""
 #log approach 
 import math
 def findNumbers(nums):
@@ -35,3 +37,7 @@ def findNumbers(nums):
         if digits % 2 == 0:
             count += 1
     return count
+"""
+Time Complexity: O(N), as calculating log_{10} is effectively O(1).
+Space Complexity: O(1).
+"""
