d6ac0bfa160d7c0e25e4a930ee5bfbc5a46958bf
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-02 17:02:24 +0530

    Update Day06_Remove_Element.py

--- Two Pointers/Day06_Remove_Element.py
+++ Two Pointers/Day06_Remove_Element.py
@@ -15,3 +15,34 @@ class Solution(object):
 sol=Solution()
 print(sol.removeElement([3,3,3,2,2,1,1],3))
 #in this we can also use swap with last element or building a new list which does not contain val value 
+class Solution(object):
+    def removeElement(self, nums, val):
+        ans = []
+
+        for num in nums:
+            if num != val:
+                ans.append(num)
+
+        for i in range(len(ans)):
+            nums[i] = ans[i]
+
+        return len(ans)
+
+
+#------------------------------------------
+class Solution(object):
+    def removeElement(self, nums, val):
+        n = len(nums)
+        i = 0
+
+        while i < n:
+            if nums[i] == val:
+                nums[i] = nums[n - 1]
+                n -= 1
+            else:
+                i += 1
+
+        return n
+
+
+
