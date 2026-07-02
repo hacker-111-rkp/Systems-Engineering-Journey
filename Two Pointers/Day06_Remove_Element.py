2ad0c8049c6fdebe11f036f02a5c8bab2cb89fe8
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-02 16:52:31 +0530

    Update Day06_Remove_Element.py

--- Two Pointers/Day06_Remove_Element.py
+++ Two Pointers/Day06_Remove_Element.py
@@ -1,1 +1,16 @@
 
+#TC--O(n)
+#SC--O(1)
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        left = 0
+
+        for right in range(len(nums)):
+            if nums[right] != val:
+                nums[left] = nums[right]
+                left += 1
+
+        return left
+sol=Solution()
+print(sol.removeElement([3,3,3,2,2,1,1],3))
