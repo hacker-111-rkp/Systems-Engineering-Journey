500114167f639282b9515bf6d38bbc1e4700d6b1
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-05 21:00:01 +0530

    Update Day09_Reverse_String.py

--- Two Pointers/Day09_Reverse_String.py
+++ Two Pointers/Day09_Reverse_String.py
@@ -1,1 +1,30 @@
 
+#two pointers 
+#TC--O(n),SC--O(1)
+class Solution(object):
+    def reverseString(self, s):
+        left=0
+        right=len(s)-1
+        while left<right:
+            s[left],s[right]=s[right],s[left]
+            left+=1
+            right-=1
+        return s
+sol=Solution()
+print(sol.reverseString(["h","e","l","l","o"]))
+
+#slicing without creating a extra space 
+#TC--O(1),SC--O(n)
+class Solution(object):
+    def reverseString(self, s):
+        s[:]=s[::-1]
+        return s
+sol=Solution()
+print(sol.reverseString(["h","e","l","l","o"]))
+#build in function of reverse
+#TC--O(n),SC--O(1)
+class Solution(object):
+    def reverseString(self, s):
+        return s.reverse()
+sol=Solution()
+print(sol.reverseString(["h","e","l","l","o"]))
