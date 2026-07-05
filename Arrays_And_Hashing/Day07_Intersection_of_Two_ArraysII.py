eeee6c556be07ca175fd07b4b2e9dd1e2a44e772
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-05 11:29:59 +0530

    Update Day07_Intersection_of_Two_ArraysII.py

--- Arrays_And_Hashing/Day07_Intersection_of_Two_ArraysII.py
+++ Arrays_And_Hashing/Day07_Intersection_of_Two_ArraysII.py
@@ -1,1 +1,36 @@
-
+#sorting + two pointers 
+#TC--O(N) , SC--O(N+M)
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        nums1.sort()
+        nums2.sort()
+        left,right=0,0
+        ans=[]
+        while left<len(nums1) and right < len(nums2):
+            if nums1[left]<nums2[right]:
+                left+=1
+            elif nums1[left]>nums2[right]:
+                right+=1
+            else:
+                ans.append(nums1[left])
+                left+=1
+                right+=1
+        return ans 
+sol=Solution()
+print(sol.intersect([9,4,4,9,8,4],[4,9,5]))
+#hashmap
+#TC--O(n+m) , SC--O(n)
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        h={}
+        ans=[]
+        for i in nums1:
+            h[i]=h.get(i,0)+1
+        for j in nums2:
+            if j in h and h[j]>0:
+                ans.append(j)
+                h[j]-=1
+        return ans
+        
+sol=Solution()
+print(sol.intersect([9,4,4,9,8,4],[4,9,5]))
