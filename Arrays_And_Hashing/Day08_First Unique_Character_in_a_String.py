2a42b46ce1429a33690ce1cc2d79a506c57df326
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-05 17:03:24 +0530

    Update Day08_First Unique_Character_in_a_String.py

--- Arrays_And_Hashing/Day08_First Unique_Character_in_a_String.py
+++ Arrays_And_Hashing/Day08_First Unique_Character_in_a_String.py
@@ -1,1 +1,36 @@
+#hashmap
+#TC--O(n),SC--O(n)
+class Solution(object):
+    def firstUniqChar(self, s):
+        h={}
+        for i in  s:
+            h[i]=h.get(i,0)+1
+        for index,value in enumerate(s):
+            if h[value]==1:
+                return index
+                break
+        return -1
+#counter
+#TC--O(n),SC--O(n)
+from collections import Counter
+class Solution(object):
+    def firstUniqChar(self, s):
+        a=Counter(s)
+        for i,j in enumerate(s) :
+            if a[j]==1:
+                return i
+        return -1
+#frequency array(26 letter) or fixed array
+#TC--O(n),SC--O(1)
+from collections import Counter
+class Solution(object):
+    def firstUniqChar(self, s):
+        count=[0]*26
+        for i in s :
+            count[ord(i)-ord('a')]+=1
+        for j,k in enumerate(s):
+            if count[ord(k)-ord('a')]==1:
+                return j
+        return -1
 
+        
