745feed278ea4da52c35739a506fb821b86589b6
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-05 20:30:44 +0530

    Update Day08_Isomorphic_Strings.py

--- Arrays_And_Hashing/Day08_Isomorphic_Strings.py
+++ Arrays_And_Hashing/Day08_Isomorphic_Strings.py
@@ -1,1 +1,28 @@
+#TWO HASH MAP
+#TC--O(n),SC--O(n)
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        h1={}
+        h2={}
+        for c1,c2 in zip(s,t):
+            if c1 in h1:
+                if h1[c1]!=c2:
+                    return False
+            else:
+                h1[c1]=c2
+            if c2 in h2:
+                if h2[c2]!=c1:
+                    return False
+            else:
+                h2[c2]=c1
+        return True
+                
+sol=Solution()
+print(sol.isIsomorphic("egg","add"))
+#ONE HASH + ONE SET
+#TC--O(),SC--O()
 
+
+
+#
+#TC--O(),SC--O()
