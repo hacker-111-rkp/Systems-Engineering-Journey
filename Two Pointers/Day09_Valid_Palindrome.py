fcd12213d9056f0c59e55ac0c6b4bf76e82f0f2c
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-05 22:28:53 +0530

    Create Day09_Valid_Palindrome.py

---
+++ Two Pointers/Day09_Valid_Palindrome.py
@@ -0,0 +1,30 @@
+#two pointers
+#TC--O(N),SC--O(N)
+class Solution(object):
+    def isPalindrome(self, s):
+        s=s.lower()
+        left=0
+        right=len(s)-1
+        while left<right:
+            if not s[left].isalnum():
+                left+=1
+            elif not s[right].isalnum():
+                right-=1
+            elif s[left]!=s[right]:
+                    return False            
+            else:
+                left+=1
+                right-=1
+        return True
+        
+#FILTER FIRST THEN CHECK
+#TC--O(N),SC--O(N)
+class Solution(object):
+    def isPalindrome(self, s):
+        filtered = ""
+
+        for ch in s.lower():
+            if ch.isalnum():
+                filtered += ch
+
+        return filtered == filtered[::-1]
