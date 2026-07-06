1038f3df178618f0e08d044741e469694f906917
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-06 19:30:21 +0530

    Update Day10_Reverse_vowels_in_string.py

--- Two Pointers/Day10_Reverse_vowels_in_string.py
+++ Two Pointers/Day10_Reverse_vowels_in_string.py
@@ -1,2 +1,21 @@
-# 
-#TC--O(),SC--O()
+# two pointers
+#TC--O(n),SC--O(n)
+class Solution(object):
+    def reverseVowels(self, s):
+        left,right=0,len(s)-1
+        vowels="aeiouAEIOU"
+        s=list(s)
+        while left<right:
+            if s[left] not in vowels:
+                left+=1
+            elif s[right] not in vowels :
+                right-=1
+            else:
+                s[left],s[right]=s[right],s[left]
+                left+=1
+                right-=1
+        return "".join(s)
+        
+            
+
+        
