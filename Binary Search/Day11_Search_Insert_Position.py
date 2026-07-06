b1689a3c5db2fc31ef15759529cc760a8e5dfea4
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-06 22:02:56 +0530

    Update Day11_Search_Insert_Position.py

--- Binary Search/Day11_Search_Insert_Position.py
+++ Binary Search/Day11_Search_Insert_Position.py
@@ -1,2 +1,20 @@
-#
-#TC--O(),SC--O()
+
+#TC--O(logn),SC--O(1)
+class Solution(object):
+    def searchInsert(self, nums, target):
+        low=0
+        high=len(nums)-1
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]>target:
+                high=mid-1
+            elif nums[mid]<target:
+                low=mid+1
+        return low
+
+                       
+sol=Solution()
+print(sol.searchInsert([1,3,5,7,9],7))
+        
