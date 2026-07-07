440d4deea11ad565bfdd732d5608cec9891937e2
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-07 09:41:34 +0530

    Update Day11_Search_in_Rotated_Sorted_Array.py

--- Binary Search/Day11_Search_in_Rotated_Sorted_Array.py
+++ Binary Search/Day11_Search_in_Rotated_Sorted_Array.py
@@ -1,2 +1,25 @@
-#
-#Tc--o(),sc--o()
+class Solution(object):
+    def search(self, nums, target):
+        low,high=0,len(nums)-1
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]==target:
+                return mid
+            #left sorted array
+            if nums[low]<=nums[mid]:
+                if  target<nums[low] or nums[mid]<target:
+                    low=mid+1
+                else:
+                    high=mid-1
+            # right sorted array 
+            if nums[high]>=nums[mid]:
+                if target < nums[mid] or target> nums[high]:
+                    high=mid-1
+                else:
+                    low=mid+1
+ 
+        return -1
+                
+
+                    
+        
