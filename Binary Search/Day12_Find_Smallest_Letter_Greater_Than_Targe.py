b3872851c013d961be7775b62cf0fda582881212
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-07 21:08:10 +0530

    Create Day12_Find_Smallest_Letter_Greater_Than_Targe.py

---
+++ Binary Search/Day12_Find_Smallest_Letter_Greater_Than_Targe.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def nextGreatestLetter(self, letters, target):
+        low = 0
+        high = len(letters) - 1
+        result = letters[0]  # Handles wrap-around automatically
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if letters[mid] > target:
+                result = letters[mid]
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return result
