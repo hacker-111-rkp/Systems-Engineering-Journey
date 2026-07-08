ba7c3c6d69199480c1096732eab9bea75fc3212f
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-08 23:01:43 +0530

    Update Day12_Find_the_Smallest_Divisor_Given _a_Threshold.py

--- Binary Search/Day12_Find_the_Smallest_Divisor_Given _a_Threshold.py
+++ Binary Search/Day12_Find_the_Smallest_Divisor_Given _a_Threshold.py
@@ -1,2 +1,20 @@
-#
-#TC--O(),SC--O()
+import math
+class Solution(object):
+    def smallestDivisor(self, nums, threshold):
+        low,high=1,max(nums)
+        result=high
+        divisor=1
+        while low<=high:
+            mid=(low+high)//2
+            a=0
+            for n in nums:
+                a+=math.ceil(float(n)/mid)
+            if a<=threshold:
+                result=mid
+                high=mid-1
+            else:
+                low=mid+1
+        return result
+
+
+        
