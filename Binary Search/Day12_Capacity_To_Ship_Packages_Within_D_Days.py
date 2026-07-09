7bc8f474d9952011f9aba8c87f157bc02e0d1c5e
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-09 13:08:32 +0530

    Update Day12_Capacity_To_Ship_Packages_Within_D_Days.py

--- Binary Search/Day12_Capacity_To_Ship_Packages_Within_D_Days.py
+++ Binary Search/Day12_Capacity_To_Ship_Packages_Within_D_Days.py
@@ -1,1 +1,24 @@
-
+class Solution(object):# in this ships == no of days 
+    def shipWithinDays(self, weights, days):
+        low=max(weights)
+        high =sum(weights)
+        result =high
+        def function(mid):
+            ships,capacity=1,mid
+            for w in weights:
+                if capacity -w<0:
+                    ships+=1
+                    capacity=mid
+                capacity -=w
+            return  ships<=days
+        while low<=high:
+            mid=(low+high)//2
+            if function(mid):
+                result=min(mid,result)
+                high=mid-1
+            else:
+                low=mid+1
+        return result
+            
+                
+        
