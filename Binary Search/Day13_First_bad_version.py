986dca8e22be0e60d6f3225a0a00d5c652edd0c1
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-09 16:03:40 +0530

    Update Day13_First_bad_version.py

--- Binary Search/Day13_First_bad_version.py
+++ Binary Search/Day13_First_bad_version.py
@@ -1,1 +1,15 @@
-
+class Solution(object):
+    def firstBadVersion(self, n):
+        low=1
+        high=n
+        result1=high
+        while low<=high:
+            mid=(low+high)//2
+            result=isBadVersion(mid)
+            if result is True:
+                high=mid-1
+                result1=min(mid,result1)
+            else:
+                low=mid+1
+        return result1
+        
