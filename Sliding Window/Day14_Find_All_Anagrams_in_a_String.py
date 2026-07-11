359c8f8168e2890f63970f1245a9bc2eb4b52a7a
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-11 22:46:54 +0530

    Update Day14_Find_All_Anagrams_in_a_String.py

--- Sliding Window/Day14_Find_All_Anagrams_in_a_String.py
+++ Sliding Window/Day14_Find_All_Anagrams_in_a_String.py
@@ -1,1 +1,26 @@
+class Solution(object):
+    def findAnagrams(self, s, p):
+        if len(p)>len(s):
+            return []
+        k=len(p)
+        h1={}
+        h2={}
+        re=[]
+        # for p in h2 frequency
+        for j in p[:]:
+            h2[j]=h2.get(j,0)+1
+        # for 1st window in h1
+        for j in s[:k]:
+            h1[j]=h1.get(j,0)+1
+        if h1==h2:
+            re.append(0)
+        # all windows frequency
+        for i in range(k,len(s)):
+            h1[s[i]]=h1.get(s[i],0)+1
+            h1[s[i-k]]-=1
+            if h1[s[i-k]] == 0:
+                del h1[s[i-k]]
+            if h1==h2:
+                re.append(i-k+1)
+        return re
 
