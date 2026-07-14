d08f45fb9514597374b7d2f9487c5b1df038ef8a
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-14 17:17:29 +0530

    Update Day15_Subarray_Product_Less_Than_K.py

--- Sliding Window/Day15_Subarray_Product_Less_Than_K.py
+++ Sliding Window/Day15_Subarray_Product_Less_Than_K.py
@@ -1,1 +1,20 @@
 
+class Solution(object):
+    def numSubarrayProductLessThanK(self, nums, k):
+        left=0
+        product =1
+        count=0
+        if k<=1:
+            return 0
+        for right in range(len(nums)):
+            product *= nums[right]
+
+            while product >= k:
+                product //= nums[left]
+                left += 1
+
+            count += (right - left + 1)
+        
+        return count
+
+        
