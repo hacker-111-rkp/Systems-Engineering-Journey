52830aa33e3b2274c229fc2ff9f82fb75fb162ed
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-14 22:03:04 +0530

    Update Day14_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length.py

--- Sliding Window/Day14_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length.py
+++ Sliding Window/Day14_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length.py
@@ -1,1 +1,20 @@
+class Solution(object):
+    def maxVowels(self, s, k):
+        vowels={'a','e','i','o','u'}
+        count=0
+        for i in s[:k]:
+            if i in vowels :
+                count+=1
+        result=count
+        for i in range(k,len(s)):
+            if s[i] in vowels:
+                count+=1
+            if s[i-k] in vowels:
+                count-=1
+            result=max(result,count)
+        return result
+
+
+        
+
 
