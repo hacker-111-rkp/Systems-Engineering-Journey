0b8ec2f1428cb2b117ca3e7077324cac909a849a
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-14 22:02:13 +0530

    Update Day10_Longest_Substring_Without_Repeating_Characters.py

--- Sliding Window/Day10_Longest_Substring_Without_Repeating_Characters.py
+++ Sliding Window/Day10_Longest_Substring_Without_Repeating_Characters.py
@@ -1,2 +1,17 @@
-#
-#TC--O(),SC--O()
+#Right pointer expands the window; left pointer shrinks it until the window becomes valid again.
+class Solution(object):
+        
+    def lengthOfLongestSubstring(self, s):
+        left = 0
+        h = {}
+        result = 0
+        for right in range(len(s)):
+        # Add current character
+            h[s[right]]=h.get(s[right],0)+1
+        # While window is invalid
+            while h[s[right]]>1:
+                h[s[left]]-=1
+                left+=1
+        
+            result=max(right-left+1,result)
+        return result
