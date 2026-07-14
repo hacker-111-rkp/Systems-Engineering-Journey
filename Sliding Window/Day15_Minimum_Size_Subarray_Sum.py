cada30318fc94ce4e7bb550a1ac5c7868c362bb5
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-14 16:39:54 +0530

    Update Day15_Minimum_Size_Subarray_Sum.py

--- Sliding Window/Day15_Minimum_Size_Subarray_Sum.py
+++ Sliding Window/Day15_Minimum_Size_Subarray_Sum.py
@@ -1,1 +1,19 @@
+class Solution(object):
+    def minSubArrayLen(self, target, nums):
+        left=0
+        window=0
+        result=float('inf')
+        for right in range(len(nums)):            
+            window+=nums[right]
+            while window>=target:
+                result=min(result,right-left+1)
+                window-=nums[left]
+                left+=1
+                
+        if result == float('inf'):
+            return 0
+        else:
+            return result 
 
+         
+        
