806976b7a2de2bc7a8464c67d28b554412dbec7f
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-15 00:08:13 +0530

    Update Day16_Binary_Subarrays_With_Sum.py

--- Sliding Window/Day16_Binary_Subarrays_With_Sum.py
+++ Sliding Window/Day16_Binary_Subarrays_With_Sum.py
@@ -1,3 +1,4 @@
+#tough one 
 class Solution(object):
     def numSubarraysWithSum(self, nums, goal):
         def atmost(goal):
@@ -13,5 +14,6 @@ class Solution(object):
                     left+=1            
                 count += (right - left + 1)
             return count
-        return atmost(goal)-atmost(goal-1)
+        return atmost(goal)-atmost(goal-1)# as atmost(2)-->0,1,2 includes all subarrays but atmost(1)-->include 0,1 subarray 
+        #so for window==2 in ex 1) stmost(2)-atmost(1)
         
