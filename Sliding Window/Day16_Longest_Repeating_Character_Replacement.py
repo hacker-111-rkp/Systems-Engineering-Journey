e3cb0d6da7ef4479bdbe3f7e5ec88f98f048e679
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-14 22:01:15 +0530

    Update Day16_Longest_Repeating_Character_Replacement.py

--- Sliding Window/Day16_Longest_Repeating_Character_Replacement.py
+++ Sliding Window/Day16_Longest_Repeating_Character_Replacement.py
@@ -1,1 +1,20 @@
 
+class Solution(object):
+    def characterReplacement(self, s, k):
+        left=0
+        h={}
+        count=0
+        result=0
+        for right in range(len(s)):
+            h[s[right]]=h.get(s[right],0)+1
+            count=max(count,h[s[right]])
+            #window_size=right-left+1, here count is max_frequency            
+            
+            while (right-left+1)-count>k:
+                h[s[left]]-=1
+                if h[s[left]]==0:
+                    del h[s[left]]
+                left+=1
+            result=max(result,right-left+1)
+        return result
+        
