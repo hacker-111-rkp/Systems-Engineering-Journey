1c9bbcd09c535cc86c4cb5136ea429b6de28b2a2
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-14 23:19:50 +0530

    Update Day16_Longest_Subarray_of_1's_After_Deleting_One_Element.py

--- Sliding Window/Day16_Longest_Subarray_of_1's_After_Deleting_One_Element.py
+++ Sliding Window/Day16_Longest_Subarray_of_1's_After_Deleting_One_Element.py
@@ -1,1 +1,17 @@
-
+class Solution(object):
+    def longestSubarray(self, nums):
+        h={}
+        result=0
+        left=0
+        for right in range(len(nums)):
+            h[nums[right]]=h.get(nums[right],0)+1
+                
+            while h.get(0,0)>1:
+                h[nums[left]]-=1
+                if h[nums[left]]==0:
+                    del h[nums[left]]
+                left+=1
+            result=max(result,right-left)# as we have to delete 0
+        return result
+        
+        
