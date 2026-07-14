36ae35aa7a7494eb81a325f43b2db6be60edf190
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-14 11:01:49 +0530

    Update Day15._Minimum_Recolors_to_Get_K_Consecutive_Black_Blockspy.py

--- Sliding Window/Day15._Minimum_Recolors_to_Get_K_Consecutive_Black_Blockspy.py
+++ Sliding Window/Day15._Minimum_Recolors_to_Get_K_Consecutive_Black_Blockspy.py
@@ -1,1 +1,25 @@
+class Solution(object):
+    def minimumRecolors(self, blocks, k):
+        count=0
+        left=0
+        h={}
+        count=len(blocks)
+        # for 1st window
+        for right in range(k):
+            h[blocks[right]]=h.get(blocks[right],0)+1
+        count=min(count,h.get('W',0))
+        for right in range(k,len(blocks)):
+            h[blocks[right]]=h.get(blocks[right],0)+1
+            if right-left+1>k:
+                h[blocks[left]]-=1
+                if h[blocks[left]]==0:
+                    del h[blocks[left]]
+                left+=1
+            count=min(count,h.get('W',0))
+        return count
+    
+            
+            
 
+
+        
