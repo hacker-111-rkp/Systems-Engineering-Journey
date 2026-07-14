8ab1d7333cf19c54d4b0cd9bda028c3e7114a9d8
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-15 00:50:56 +0530

    Update Day16_Count_Number_of_Nice_Subarrays.py

--- Sliding Window/Day16_Count_Number_of_Nice_Subarrays.py
+++ Sliding Window/Day16_Count_Number_of_Nice_Subarrays.py
@@ -1,1 +1,22 @@
 
+class Solution(object):
+    def numberOfSubarrays(self, nums, k):
+        def atmost(k):
+            count=0
+            left=0
+            window=0
+            result=0
+            for right in range(len(nums)):
+                if nums[right]%2!=0:
+                    count+=1
+                while count>k:                   
+                    window-=nums[left]
+                    if nums[left] % 2 != 0:
+                        count -= 1
+                    left+=1
+                result+=(right-left+1)
+            return result
+        return atmost(k)-atmost(k-1)
+                
+        return result
+        
