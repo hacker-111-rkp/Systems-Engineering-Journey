3aa549d6953ff9ab5aa3d3f54f74f483b3ecc8f8
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-15 01:09:16 +0530

    Update Day16_Subarrays_with_K_Different_Integers.py

--- Sliding Window/Day16_Subarrays_with_K_Different_Integers.py
+++ Sliding Window/Day16_Subarrays_with_K_Different_Integers.py
@@ -1,3 +1,10 @@
+#note the pattern of this atmost 
+'''Ask yourself:
+Can I easily count "at most K", but the question wants "exactly K"?
+
+After I make my window valid, how many valid subarrays end at right?
+Count of valid subarrays--->result += (right - left + 1)
+'''
 class Solution(object):
     def subarraysWithKDistinct(self, nums, k):
         def atmost(k):
@@ -11,7 +18,7 @@ class Solution(object):
                     if h[nums[left]]==0:
                         del h[nums[left]]
                     left+=1
-                result+=(right-left+1)
+                result+=(right-left+1)# gives all the subarray sum 
             return result
         return atmost(k)-atmost(k-1)
         
