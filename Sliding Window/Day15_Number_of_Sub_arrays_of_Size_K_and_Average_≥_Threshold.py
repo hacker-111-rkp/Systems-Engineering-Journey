af48f6f6353c0ce2435fce155c89bad98dee1482
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-14 15:10:26 +0530

    Update Day15_Number_of_Sub_arrays_of_Size_K_and_Average_≥_Threshold.py

--- Sliding Window/Day15_Number_of_Sub_arrays_of_Size_K_and_Average_≥_Threshold.py
+++ Sliding Window/Day15_Number_of_Sub_arrays_of_Size_K_and_Average_≥_Threshold.py
@@ -1,1 +1,23 @@
+class Solution(object):
+    def numOfSubarrays(self, arr, k, threshold):
+        left=0
+        window=0
+        count=0
+        #for 1st window
+        for right in range(k):
+            window+=arr[right]
+        average=window//k
+        if average>=threshold:
+            count+=1
+        #for remaining window
+        for right in range(k,len(arr)):
+            window+=arr[right]
+            if right-left+1 >k:
+                window-=arr[left]
+                left+=1
+            average=window//k
+            if average>=threshold:
+                count+=1
+        return count
 
+        
