83111d194292c02e2393403b5cc351c57068da32
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-15 01:01:05 +0530

    Update Day16_Subarrays_with_K_Different_Integers.py

--- Sliding Window/Day16_Subarrays_with_K_Different_Integers.py
+++ Sliding Window/Day16_Subarrays_with_K_Different_Integers.py
@@ -1,1 +1,17 @@
-
+class Solution(object):
+    def subarraysWithKDistinct(self, nums, k):
+        def atmost(k):
+            h={}
+            left=0
+            result=0
+            for right in range(len(nums)):
+                h[nums[right]]=h.get(nums[right],0)+1
+                while len(h)>k:
+                    h[nums[left]]-=1
+                    if h[nums[left]]==0:
+                        del h[nums[left]]
+                    left+=1
+                result+=(right-left+1)
+            return result
+        return atmost(k)-atmost(k-1)
+        
