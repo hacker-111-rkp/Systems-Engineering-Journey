0a4c336a9ea9a69d25df8d15f0c0cc71ab36c11a
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-15 17:34:09 +0530

    Update Day18_Range_Sum_Query.py

--- Prefix Sum /Day18_Range_Sum_Query.py
+++ Prefix Sum /Day18_Range_Sum_Query.py
@@ -1,1 +1,10 @@
 
+
+class NumArray(object):
+    def __init__(self, nums):#__init__ never return anything 
+        self.nums=nums    # nums now is stored in object    
+    def sumRange(self, left, right):#---o(1)
+        total=0
+        for i in range(left,right+1):#o(n)
+            total+=self.nums[i]
+        return total
