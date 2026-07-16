f15cbc7634baa9ee9cc60ccc199266d109d533dc
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-16 11:29:34 +0530

    Update Day19_Left_and_Right_Sum_Differences.py

--- Prefix Sum /Day19_Left_and_Right_Sum_Differences.py
+++ Prefix Sum /Day19_Left_and_Right_Sum_Differences.py
@@ -1,1 +1,18 @@
 
+class Solution(object):
+    def leftRightDifference(self, nums):
+        left=0
+        total=sum(nums)
+        re=[]
+        leftSum=[0]*len(nums)
+        rightSum=[0]*len(nums)
+        for i in range(len(nums)):
+            right=total-nums[i]-left
+            
+            re.append(abs(right-left))
+            left+=nums[i]
+        return re 
+             
+
+        
+        
