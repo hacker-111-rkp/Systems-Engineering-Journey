a66faa5980a4832e528fd149d3db1b11c73fa6a2
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-16 19:44:37 +0530

    Update Day19_Contiguous_Array.py

--- Prefix Sum /Day19_Contiguous_Array.py
+++ Prefix Sum /Day19_Contiguous_Array.py
@@ -1,1 +1,17 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        h={0:-1}
+        prefix=0
+        count=0
+        for i in range(len(nums)):
+
+            if nums[i]==0:
+                prefix-=1
+            else:
+                prefix+=1
+            if prefix in h:
+                count=max(count,i-h[prefix])
+            else:
+                h[prefix]=i
+        return count
 
