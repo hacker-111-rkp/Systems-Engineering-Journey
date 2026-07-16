043077dbb17cb43ec0118835b7b4bc4e1d0f9a7e
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-16 10:42:43 +0530

    Update Day19_Find_the_Highest_Altitude.py

--- Prefix Sum /Day19_Find_the_Highest_Altitude.py
+++ Prefix Sum /Day19_Find_the_Highest_Altitude.py
@@ -1,1 +1,10 @@
+#o(n),o(1)
+#resolved as prefix sum problem 
+class Solution(object):
+    def largestAltitude(self, gain):
+        gain.insert(0,0)
+        for i in range(1,len(gain)):
+            gain[i]+=gain[i-1]
 
+        return max(gain)
+        
