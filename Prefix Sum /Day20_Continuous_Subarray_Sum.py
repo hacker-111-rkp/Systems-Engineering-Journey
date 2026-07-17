ba8005d6fcfb52f18bbcfbe820e82bae14081f93
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-17 16:05:31 +0530

    Update Day20_Continuous_Subarray_Sum.py

--- Prefix Sum /Day20_Continuous_Subarray_Sum.py
+++ Prefix Sum /Day20_Continuous_Subarray_Sum.py
@@ -1,1 +1,14 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        prefix=0
+        h={0:-1}
+        for i in range(len(nums)):
+            prefix+=nums[i]
+            rem=prefix%k
+            if rem in h:
+                if i-h[rem]>=2:
+                    return True
+            else:
+                h[rem]=i
+        return False
 
