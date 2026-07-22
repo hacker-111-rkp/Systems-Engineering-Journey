83e84cf37e93dc2214065feb785d6f522cc17705
hacker-111-rkp <hackerkp76@gmail.com>
2026-07-23 01:46:07 +0530

    Update Day22_Middle_of_the_Linked_List.py

--- Linked List/Day22_Middle_of_the_Linked_List.py
+++ Linked List/Day22_Middle_of_the_Linked_List.py
@@ -1,1 +1,13 @@
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:        
+         
+        nodes=[]
+        curr=head
+        while curr:
+            nodes.append(curr)
+            curr=curr.next
+        mid=len(nodes)//2
+        return nodes[mid]
+        
+
 
