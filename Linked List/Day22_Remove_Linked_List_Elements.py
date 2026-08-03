080d62f399100c280163e0446101d5ffdae7232a
hacker-111-rkp <hackerkp76@gmail.com>
2026-08-03 17:29:42 +0530

    Update Day22_Remove_Linked_List_Elements.py

--- Linked List/Day22_Remove_Linked_List_Elements.py
+++ Linked List/Day22_Remove_Linked_List_Elements.py
@@ -1,1 +1,21 @@
 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeElements(self, head, val):
+        if head is None:
+            return None
+        while head and head.val == val:
+            head=head.next
+        curr=head
+        while curr and curr.next:
+            if curr.next.val!=val:
+                curr=curr.next
+            else:
+                curr.next=curr.next.next
+        return head
+
+        
