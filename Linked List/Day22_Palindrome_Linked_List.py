0b3bd9f355958c659440f62f0ca04cf54d53ef19
hacker-111-rkp <hackerkp76@gmail.com>
2026-08-03 17:53:30 +0530

    Update Day22_Palindrome_Linked_List.py

--- Linked List/Day22_Palindrome_Linked_List.py
+++ Linked List/Day22_Palindrome_Linked_List.py
@@ -1,1 +1,19 @@
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        if head is None:
+            return True
+        curr=head
+        values=[]
+        while curr:
+            values.append(curr.val)
+            curr=curr.next
+        return values[::-1]==values
+    
+
+            
