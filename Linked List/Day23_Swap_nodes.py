c006e14d3159baf1e9b465a1a0236a2637631ca2
hacker-111-rkp <hackerkp76@gmail.com>
2026-08-05 18:30:54 +0530

    Create Day23_Swap_nodes.py

---
+++ Linked List/Day23_Swap_nodes.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+#note you cant relink the nodes values only the nodes can relink 
+class Solution(object):
+    def swapPairs(self, head):
+        if head is None:
+            return None
+        curr=head
+        dummy=ListNode()
+        dummy.next=head
+        prev=dummy
+        while curr and curr.next:
+            temp=curr.next
+            new=temp.next
+            temp.next=curr
+            prev.next=temp
+            curr.next=new
+            prev=curr
+            curr=curr.next
+            
+        return dummy.next
+
+        
